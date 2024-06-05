e4f694de57bc6e8cb9c86aa18457618bf4f36187
SimonKefas <simon@nettskaperne.no>
2024-06-05 02:23:45 +0200

    Adding period control, and foul control

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ let guestScoreElement = document.getElementById("guestScore");
 let homeScore = 0;
 let guestScore = 0;
 
+let foulCounterHome = document.getElementById("foul-counter-home");
+let foulCounterGuest = document.getElementById("foul-counter-guest");
+
+let periodCounter = document.getElementById("period-counter")
+
+let foulsHome = 0;
+let foulsGuest = 0;
+
+let period = 1;
+
 function checkLeadingTeam() {
     if (homeScore > guestScore) {
         guestScoreElement.classList.remove("current-leader");
@@ -27,9 +37,26 @@ function gameCounter(number, scoreName) {
         homeScore += number;
         homeScoreElement.textContent = homeScore;
     } else {
-        console.error("Something wrong happened!")
+        console.error("Something wrong happened with the gameCounter function: " + error)
     }
     checkLeadingTeam()
+}
+
+function foulCalculate(number, scoreName) {
+    if (scoreName === 'guest') {
+        foulsGuest += number;
+        foulCounterGuest.textContent = foulsGuest;
+    } else if (scoreName === 'home') {
+        foulsHome += number;
+        foulCounterHome.textContent = foulsHome;
+    } else {
+        console.error("Something wrong happened with the foulCalculate function: " + error)
+    }
+}
+
+function periodCalculate(number) {
+    period += number;
+    periodCounter.textContent = "Period: " + period;
 }
 
 function newGame() {
@@ -37,4 +64,14 @@ function newGame() {
     guestScore = 0;
     homeScoreElement.textContent = homeScore;
     guestScoreElement.textContent = guestScore;
+
+    foulsGuest = 0;
+    foulsHome = 0;
+    foulCounterGuest.textContent = foulsGuest;
+    foulCounterHome.textContent = foulsHome;
+
+    period = 1;
+    periodCounter.textContent = "Period: " + period;
+
+    checkLeadingTeam()
 }
